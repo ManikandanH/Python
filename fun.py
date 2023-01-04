133aa448d9db99a8d1d36665787caa4e72de447f
Manikandan Arjunan <ronaldomanicr7@gmail.com>
2023-01-04 21:24:18 +0530

    python functions

--- fun.py
+++ fun.py
@@ -1,6 +1,30 @@
 def helloWorld(name, emoji):
     print(f'Hello World! by {name} {emoji}')
-    
+
+
 helloWorld('Manikandan', ':)')
 
 
+def funcWithArgs(*args, **kargs):
+    return sum(args) + sum(kargs.values())
+
+
+print(funcWithArgs(1, 2, 3, 4, 5, num1=5, num2=10))
+
+
+def highestEven(li):
+    maxEven = 0
+    for element in li:
+        if element % 2 == 0:
+            maxEven = maxEven if maxEven > element else element
+    return maxEven
+
+
+print(highestEven([10, 2, 3, 4, 8, 11]))
+
+def sum():
+    return 'asd'
+
+print(sum())
+a = 'asdasdasdasdoo'
+print(a[:-1])
